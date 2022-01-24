e0a99c9a97fbc00b4ec5b3531a83d0902475d14b
Konstantin Kasterov <endimion-xi@yandex.ru>
2022-01-24 14:36:07 +0300

    App with @PostConstruct and @PreDestroy

---
+++ src/main/java/ru/kasterov/spring/spring_introduction/ScopeTest.java
@@ -0,0 +1,18 @@
+package ru.kasterov.spring.spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ScopeTest {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
+
+        Dog myDog = context.getBean("dogBean", Dog.class);
+        Dog yourDog = context.getBean("dogBean", Dog.class);
+
+        System.out.println("Переменные ссылаются на один и тот же объект? " + (myDog == yourDog));
+        System.out.println(myDog);
+        System.out.println(yourDog);
+
+        context.close();
+    }
+}
