4f91237839c2bde7b45ba18b0842e557a998b8fa
Konstantin Kasterov <endimion-xi@yandex.ru>
2022-01-24 15:10:10 +0300

    App with Java code 1st version

---
+++ src/main/java/ru/kasterov/spring/spring_introduction/Test6.java
@@ -0,0 +1,15 @@
+package ru.kasterov.spring.spring_introduction;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test6 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Person person = context.getBean("personBean", Person.class);
+        person.callYourPet();
+
+        context.close();
+
+    }
+}
