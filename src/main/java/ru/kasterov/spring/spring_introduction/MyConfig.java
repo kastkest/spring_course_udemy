012dd73acffe254c574869632e5fed473b52b8e2
Konstantin Kasterov <endimion-xi@yandex.ru>
2022-01-24 15:29:03 +0300

    App with @PropertySource

--- src/main/java/ru/kasterov/spring/spring_introduction/MyConfig.java
+++ src/main/java/ru/kasterov/spring/spring_introduction/MyConfig.java
@@ -1,11 +1,9 @@
 package ru.kasterov.spring.spring_introduction;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.*;
 
 @Configuration
+@PropertySource("classpath:myApp.properties")
 public class MyConfig {
 
     @Bean
