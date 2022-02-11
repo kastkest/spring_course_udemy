46a684bc97a622cde3cd791f47ccde5bf65f6b65
kastkest <endimion-xi@yandex.ru>
2022-02-11 17:30:41 +0300

    Create simple Spring Dep

---
+++ src/main/java/ru/kasterov/spring/aop/MyConfig.java
@@ -0,0 +1,9 @@
+package ru.kasterov.spring.aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.kasterov.spring.aop")
+public class MyConfig {
+}
