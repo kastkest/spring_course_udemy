acbcf0a4668d73562ff5bc3e682cf4159d42b283
kastkest <endimion-xi@yandex.ru>
2022-02-11 18:30:52 +0300

    Add PointCuts

--- src/main/java/ru/kasterov/spring/aop/AbstractLibrary.java
+++ src/main/java/ru/kasterov/spring/aop/AbstractLibrary.java
@@ -1,5 +1,5 @@
 package ru.kasterov.spring.aop;
 
 public abstract class AbstractLibrary {
-    abstract public void getBook();
+//    abstract public void getBook();
 }
--- src/main/java/ru/kasterov/spring/aop/UniLibrary.java
+++ src/main/java/ru/kasterov/spring/aop/UniLibrary.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UniLibrary extends AbstractLibrary {
-    @Override
+
     public void getBook() {
         System.out.println("Мы берем книгу из UniLibrary");
     }
