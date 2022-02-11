711f427eef8a30c8f95c81d64b2d6e86eecd66b9
kastkest <endimion-xi@yandex.ru>
2022-02-11 17:41:59 +0300

    Add Before Advice

--- src/main/java/ru/kasterov/spring/aop/MyConfig.java
+++ src/main/java/ru/kasterov/spring/aop/MyConfig.java
@@ -2,8 +2,10 @@ package ru.kasterov.spring.aop;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan("ru.kasterov.spring.aop")
+@EnableAspectJAutoProxy
 public class MyConfig {
 }
---
+++ src/main/java/ru/kasterov/spring/aop/aspects/LoggingAspect.java
@@ -0,0 +1,15 @@
+package ru.kasterov.spring.aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Before("execution(public void getBook())")
+    public void beforeGetBookAdvice() {
+        System.out.println("beforeGetBookAdvice(): попытка получить книгу");
+    }
+}
